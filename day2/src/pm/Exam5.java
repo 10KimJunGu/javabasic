56aa07fb12166b6c5cdd9d03da681b7ac926fe44
32KimJunGu <98381542+32KimJunGu@users.noreply.github.com>
2022-03-03 16:49:39 +0900

    33

---
+++ day2/src/pm/Exam5.java
@@ -0,0 +1,35 @@
+package pm;
+
+public class Exam5 {
+
+	public static void main(String[] args) {
+		
+		
+//		quiz 2
+		
+		
+//		int mon = 3;
+//		
+//		switch(mon / 3) {
+//		case 1:
+//			System.out.println("봄입니다");
+//			break;
+//		case 2:
+//			System.out.println("여름입니다");
+//			break;
+//		case 3:
+//			System.out.println("가을입니다");
+//			break;
+//		default:
+//			System.out.println("겨울입니다.");
+//		}
+		
+		
+		
+
+		
+		
+		
+	}
+
+}
---
+++ day2/src/pm/Exam6.java
@@ -0,0 +1,175 @@
+package pm;
+
+import java.util.Scanner;
+
+public class Exam6 {
+	public static void main(String[] args) {
+		
+		
+		
+//		quiz 3
+		
+		
+		
+		//내가 한것
+		
+//		String qMsg = "달 숫자를 넣으세요.";
+//		
+//		int mon = 0;
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println(qMsg + "\n" + mon);
+//		
+//		mon = sc.nextInt();
+//		
+//		switch(mon) {
+//		case 1:
+//			System.out.println("Jan");
+//			break;
+//		case 2:
+//			System.out.println("Feb");
+//			break;
+//		case 3:
+//			System.out.println("Mar");
+//			break;
+//		case 4:
+//			System.out.println("Apr");
+//			break;
+//		case 5:
+//			System.out.println("May");
+//			break;
+//		case 6:
+//			System.out.println("Jun");
+//			break;
+//		case 7:
+//			System.out.println("Jul");
+//			break;
+//		case 8:
+//			System.out.println("Aug");
+//			break;
+//		case 9:
+//			System.out.println("Sep");
+//			break;
+//		case 10:
+//			System.out.println("Oct");
+//			break;
+//		case 11:
+//			System.out.println("Nov");
+//			break;
+//		default :
+//			System.out.println("Dec");
+//		}
+		
+		
+		
+		
+		
+		//import 방법 : ctrl + Shift + o   -> 여러개를 모두 import
+		//            ctrl + spacebar    -> 해당 라이브러리만 import
+		
+		
+		
+		// 답지
+
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("월을 입력하세요. >>>");      //이 세줄은 외워놔라!!!!!!!
+//		int mon = sc.nextInt();
+//		
+//		
+//		switch(mon) {
+//		case 1 : 
+//			System.out.println("Jan");
+//			break;
+//		case 2:
+//			System.out.println("Feb");
+//			break;
+//		case 3:
+//			System.out.println("Mar");
+//			break;
+//		case 4:
+//			System.out.println("Apr");
+//			break;
+//		case 5:
+//			System.out.println("May");
+//			break;
+//		case 6:
+//			System.out.println("Jun");
+//			break;
+//		case 7:
+//			System.out.println("Jul");
+//			break;
+//		case 8:
+//			System.out.println("Aug");
+//			break;
+//		case 9:
+//			System.out.println("Sep");
+//			break;
+//		case 10:
+//			System.out.println("Oct");
+//			break;
+//		case 11:
+//			System.out.println("Nov");
+//			break;
+//		default :
+//			System.out.println("Dec");
+//
+//		}
+		
+		
+		
+		
+//----------------------------------------------------------------------------------------
+		
+		
+		
+		
+		//반복문 for
+		// 실행 구문을 반복적으로 수행하는 반복 제어문으로  일반적으로 반복횟수가 정확할때 주로 사용된다.
+		// 즉, for문의 구문을 보면 몇 번 반복할 지 유추할 수 있어야한다.
+		
+		//for문의 기본형태
+//		for (start; stop; step) {
+//			
+//		}
+		
+		
+//		for(int i = 0; i <= 10; i++) {
+//			System.out.println(i);
+//			
+//		}
+		
+		
+		
+		// 이중 for문
+		
+//		for (int i = 1; i <= 3; i++) {
+//			for(int j = 1; j <= 5; j++) {
+//				System.out.println(i + "일차" + j + "교시");
+//			}
+//		}
+//		
+		
+		
+		
+		// for each문
+		// array 배열 ( 리스트랑 비슷함)
+		
+		//String name = "홍길동";     변수 1개에 데이터 1개 저장	
+//		String [] names = {"홍길동", "이몽룡","성춘향"};
+//		int [] numbers = {1,2,3,4,5};
+//		
+//		for (string name : names) {
+//			System.out.println(name);
+//		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
