791b9ede617b0dbf550f0a9e8c3afec855d0ed1c
32KimJunGu <98381542+32KimJunGu@users.noreply.github.com>
2022-03-12 22:53:37 +0900

    d

--- day6/src/pm/Exam7_1.java
+++ day6/src/pm/Exam7_1.java
@@ -8,9 +8,9 @@ public class Exam7_1 {
 		//생성자의 모양(개수, 자료형)에 따라 객체를 생성하는 방법이 결정된다.
 		
 		
-		K k1 = new K();
-		K k2 = new K(3);
-		K k3 = new K(3,5);
+		K k1 = new K();   //1
+		K k2 = new K(3);   //2
+		K k3 = new K(3,5);   //3
 		
 
 	}
@@ -18,13 +18,13 @@ public class Exam7_1 {
 }
 
 class K{
-	K(){ //생성자는 클래스명과 동일, 리턴이 없다.
+	K(){ //생성자는 클래스명과 동일, 리턴이 없다.   //1
   		System.out.println("첫번째 생성자");
 	}
-	K(int a){ //생성자는 클래스명과 동일, 리턴이 없다.
+	K(int a){ //생성자는 클래스명과 동일, 리턴이 없다.   //2
   		System.out.println("두번째 생성자");
 	}
-	K(int a, int b){ //생성자는 클래스명과 동일, 리턴이 없다.
+	K(int a, int b){ //생성자는 클래스명과 동일, 리턴이 없다.   //3
   		System.out.println("세번째 생성자");
 	}
 }
